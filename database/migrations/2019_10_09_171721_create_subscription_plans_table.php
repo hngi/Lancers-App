1c842cdd5b1d90337e84a7494ce405b0b83451a9
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-10-12 06:29:10 +0100

    set back to json datatype

--- database/migrations/2019_10_09_171721_create_subscription_plans_table.php
+++ database/migrations/2019_10_09_171721_create_subscription_plans_table.php
@@ -18,7 +18,7 @@ class CreateSubscriptionPlansTable extends Migration
             $table->string('name',255);
             $table->text('description')->nullable();
             $table->json('features')->nullable();
-            $table->float('price');
+            $table->float('text');
             $table->timestamps();
         });
     }
