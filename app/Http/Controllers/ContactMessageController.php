036465acb263a79ed89b95d747a07c6ab88389a9
moneya <moneya59@gmail.com>
2019-10-13 08:14:35 +0100

    added validator

--- app/Http/Controllers/ContactMessageController.php
+++ app/Http/Controllers/ContactMessageController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\ContactMessage;
 use Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ContactMessageController extends Controller
 {
