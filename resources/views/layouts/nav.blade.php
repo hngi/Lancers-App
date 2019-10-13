5d895c75356c8c31cde818d64636206895835623
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-10-13 18:13:00 +0100

    added project list to nav and populate page with project data

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,7 +10,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-left mt-2 mt-lg-0">
               <li class="nav-item ml-3 active">
-                <a class="nav-link " href="#">Project Info <span class="sr-only">(current)</span></a>
+                <a class="nav-link " href="/dashboard/projects_list">Projects <span class="sr-only">(current)</span></a>
               </li>
              <!-- <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
